7fe7b0e6abc206604c1d766ea74a84e8c8c6a9b8
BaffledHedgehog <yaroslavlyakhov123456789@gmail.com>
2026-02-13 14:27:51 +0300

    init

---
+++ src/main/java/baffledhedgehog/innofixes/InnoFixes.java
@@ -0,0 +1,8 @@
+package baffledhedgehog.innofixes;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("innofixes")
+public class InnoFixes {
+    public static final String MOD_ID = "innofixes";
+}
